91a67ce83a3be5240335aad6c5341a3e3f722e15
Pemdas1991 <Pemdas1991@gmail.com>
2022-07-09 13:44:13 -0600

    enable clicking grid squares

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-const grid = document.querySelector(`.grid`);
 
-for (i = 0; i < 16; i++) {
-    let line = document.createElement('div');
-    line.classList.add('line');
+let size = 16;
 
-    for (j = 0; j < 16; j++) {
-        let column = document.createElement('div');
-        column.classList.add('box')
-        line.appendChild(column);
+drawGrid(size);
+
+function drawGrid(size) {
+    const grid = document.querySelector('.grid');
+
+    for (i = 0; i < size; i++) {
+        let line = document.createElement('div');
+        line.classList.add('line');
+
+        for (j = 0; j < size; j++) {
+            let column = document.createElement('div');
+            column.classList.add('box')
+            line.appendChild(column);
+        }
+
+        grid.appendChild(line);
     }
 
-    grid.appendChild(line);
+    addEventToBoxes()
 }
+
+
+function addEventToBoxes() {
+    const boxes = document.querySelectorAll('.box')
+
+    boxes.forEach(box => {
+        box.addEventListener('click', makeBlack)
+    });
+}
+
+function makeBlack(box) {
+    this.classList.add('clicked');
+}
+
